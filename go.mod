6c56dae7de956c194464ebfb915bfbe822047e3a
sudachi0114 <jnevariste@gmail.com>
2021-03-12 16:28:52 +0900

    試しに sqlite に接続して、動作確認

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sudachi0114/web-db-docker
 
 go 1.15
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/mattn/go-sqlite3 v1.14.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	db, err := gorm.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success to Connect Database.")
+	defer db.Close()
 }
