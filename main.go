47b206683a39936c09a9e7a6f8949c40be3cb102
sudachi0114 <jnevariste@gmail.com>
2021-03-14 14:07:16 +0900

    main では、gin を用いた web サーバを立てるようにする

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
