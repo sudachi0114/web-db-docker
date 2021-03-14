7b19e90b31f0e3a98bca48f072ac97e99f002ddf
sudachi0114 <jnevariste@gmail.com>
2021-03-14 14:21:43 +0900

    html でもレスポンスできるようにした (普通こういうことしないだろうけど、お勉強用なので..)

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
 
+	r.GET("/", responseWithTemplate)
 	r.GET("/ping", responseByJson)
 
 	r.Run()
@@ -18,4 +20,8 @@ func responseByJson(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+func responseWithTemplate(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
