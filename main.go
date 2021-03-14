c5c40903cd774bfd9ede424384c436a9c2d054b1
sudachi0114 <jnevariste@gmail.com>
2021-03-14 15:19:19 +0900

    go からデータベースコンテナに接続

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
+	db, err := gorm.Open("mysql", "test:passw0rd@tcp(db:3306)/test_db?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successed to connect MySQL Database!")
+	defer db.Close()
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
 
